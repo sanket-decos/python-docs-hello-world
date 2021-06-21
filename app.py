43980d67dfbc29aa334cb9d1a80f1d38fed192c8
sanket-decos <86230483+sanket-decos@users.noreply.github.com>
2021-06-21 15:17:18 +0530

    changes 21 June

--- app.py
+++ app.py
@@ -1,6 +1,82 @@
 from flask import Flask
-app = Flask(__name__)
+from flask_restful import Resource, Api, reqparse
+from flask_cors import CORS
+import pandas as pd
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+app = Flask(__name__)
+CORS(app)
+api = Api(app)
+
+class Users(Resource):
+    def get(self):
+        parser = reqparse.RequestParser()  # initialize
+        parser.add_argument('userId', required=True)  # add args
+        args = parser.parse_args()  # parse arguments to dictionary
+
+        #data = pd.read_csv('users.csv')  # read local CSV
+        df = pd.read_csv('users.csv')  # read local CSV
+        data = df.loc[df['userId'] == args['userId']]
+        data = data.to_dict()  # convert dataframe to dict
+        return {'data': data}, 200  # return data and 200 OK
+
+    def post(self):
+        parser = reqparse.RequestParser()  # initialize
+        parser.add_argument('userId', required=True)  # add args
+        parser.add_argument('name', required=True)
+        parser.add_argument('city', required=True)
+        args = parser.parse_args()  # parse arguments to dictionary
+
+        # read our CSV
+        data = pd.read_csv('users.csv')
+
+        if args['userId'] in list(data['userId']):
+            return {
+                'message': f"'{args['userId']}' already exists."
+            }, 409
+        else:
+            # create new dataframe containing new values
+            new_data = pd.DataFrame({
+                'userId': [args['userId']],
+                'name': [args['name']],
+                'city': [args['city']],
+                'locations': [[]]
+            })
+            # add the newly provided values
+            data = data.append(new_data, ignore_index=True)
+            data.to_csv('users.csv', index=False)  # save back to CSV
+            return {'data': data.to_dict()}, 200  # return data with 200 OK
+
+                    
+class CustomerSatisfactionTeams(Resource):
+    def get(self):
+        #parser = reqparse.RequestParser()  # initialize
+        #parser.add_argument('userId', required=True)  # add args
+        #args = parser.parse_args()  # parse arguments to dictionary
+
+        data = pd.read_csv('customerSatisfactionTeams.csv')  # read local CSV
+        #df = pd.read_csv('customerSatisfactionTeams.csv')  # read local CSV
+        #data = df.loc[df['userId'] == args['userId']]
+        data = data[['Team', 'Quarter', 'Rating']]
+        data = data.to_dict()  # convert dataframe to dict
+        return {'data': data}, 200  # return data and 200 OK
+
+                    
+class CustomerSatisfactionEmployees(Resource):
+    def get(self):
+        parser = reqparse.RequestParser()  # initialize
+        parser.add_argument('TeamName', required=True)  # add args
+        args = parser.parse_args()  # parse arguments to dictionary
+
+        #data = pd.read_csv('customerSatisfactionEmployees.csv')  # read local CSV
+        df = pd.read_csv('customerSatisfactionEmployees.csv')  # read local CSV
+        data = df.loc[df['TeamName'] == args['TeamName']][['Employee', 'Communication', 'Knowledge', 'TotalImpression']]
+        data = data.to_dict()  # convert dataframe to dict
+        return {'data': data}, 200  # return data and 200 OK
+
+
+api.add_resource(Users, '/users')  # add endpoints
+api.add_resource(CustomerSatisfactionTeams, '/customerSatisfactionTeams')  # add endpoints
+api.add_resource(CustomerSatisfactionEmployees, '/customerSatisfactionEmployees')  # add endpoints
+
+if __name__ == '__main__':
+    app.run()  # run our Flask app
