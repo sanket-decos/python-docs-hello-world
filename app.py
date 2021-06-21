d70a444eaef75d527b1f2a5c6bb3597b33a697a2
sanket-decos <86230483+sanket-decos@users.noreply.github.com>
2021-06-21 15:22:03 +0530

    Update 21 June

--- app.py
+++ app.py
@@ -3,49 +3,10 @@ from flask_restful import Resource, Api, reqparse
 from flask_cors import CORS
 import pandas as pd
 
+
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
-
-class Users(Resource):
-    def get(self):
-        parser = reqparse.RequestParser()  # initialize
-        parser.add_argument('userId', required=True)  # add args
-        args = parser.parse_args()  # parse arguments to dictionary
-
-        #data = pd.read_csv('users.csv')  # read local CSV
-        df = pd.read_csv('users.csv')  # read local CSV
-        data = df.loc[df['userId'] == args['userId']]
-        data = data.to_dict()  # convert dataframe to dict
-        return {'data': data}, 200  # return data and 200 OK
-
-    def post(self):
-        parser = reqparse.RequestParser()  # initialize
-        parser.add_argument('userId', required=True)  # add args
-        parser.add_argument('name', required=True)
-        parser.add_argument('city', required=True)
-        args = parser.parse_args()  # parse arguments to dictionary
-
-        # read our CSV
-        data = pd.read_csv('users.csv')
-
-        if args['userId'] in list(data['userId']):
-            return {
-                'message': f"'{args['userId']}' already exists."
-            }, 409
-        else:
-            # create new dataframe containing new values
-            new_data = pd.DataFrame({
-                'userId': [args['userId']],
-                'name': [args['name']],
-                'city': [args['city']],
-                'locations': [[]]
-            })
-            # add the newly provided values
-            data = data.append(new_data, ignore_index=True)
-            data.to_csv('users.csv', index=False)  # save back to CSV
-            return {'data': data.to_dict()}, 200  # return data with 200 OK
-
                     
 class CustomerSatisfactionTeams(Resource):
     def get(self):
