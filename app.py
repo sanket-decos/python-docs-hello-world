957422baa55bcf337c4f4a4cbb8e64db8b3204b7
sanket-decos <86230483+sanket-decos@users.noreply.github.com>
2021-06-22 19:16:59 +0530

    Update 22 June

--- app.py
+++ app.py
@@ -199,6 +199,110 @@ class CSAllOpenTicketsTicketsByCompanyname(Resource):
         return {'data': data}, 200  # return data and 200 OK
 
 
+
+
+class CSClosedTicketsTicketsByAgentMessage(Resource):
+    def get(self):
+
+        df = pd.read_csv('customerSupport.csv')  # read local CSV
+        #Apply filters
+        df = df[df.status.isin(['Closed', 'In ontwikkeling', 'Resolved'])]
+        df = df[df.message.isin(['Feedback', 'Incident / storing', 'Project', 'Vraag', ])]
+        #Group by
+        datagroup = df.groupby(['agent', 'message']).size().reset_index(name='countt')
+        pivoted = datagroup.pivot(index='agent', columns= 'message', values='countt')
+        flattened = pd.DataFrame(pivoted.to_records()).fillna(0)
+        data = flattened.to_dict()  # convert dataframe to dict
+        
+        return {'data': data}, 200  # return data and 200 OK
+
+                    
+class CSClosedTicketsTicketsByGroup(Resource):
+    def get(self):
+
+        df = pd.read_csv('customerSupport.csv')  # read local CSV
+        #Apply filters
+        df = df[df.status.isin(['Closed', 'In ontwikkeling', 'Resolved'])]
+        df = df[df.message.isin(['Feedback', 'Incident / storing', 'Project', 'Vraag', ])]
+
+        def ClubFunction(row):
+            if row['groupName'] == 'Development':
+                groupNameClubbed = 'Development'
+            elif row['groupName'] == 'Technisch consultants':
+                groupNameClubbed = 'Technisch consultants and Functioneel consultants'
+            elif row['groupName'] == 'Functioneel consultants':
+                groupNameClubbed = 'Technisch consultants and Functioneel consultants'
+            elif row['groupName'] == 'Intake':
+                groupNameClubbed = 'Intake'
+            elif row['groupName'] == 'India Intake':
+                groupNameClubbed = 'Intake'
+            elif row['groupName'] == 'Support 1e lijn':
+                groupNameClubbed = 'Support 1e lijn'
+            elif row['groupName'] == 'Support 2e lijn':
+                groupNameClubbed = 'Support 2e lijn'
+            else:
+                groupNameClubbed = 'Others'
+            return groupNameClubbed
+        df['groupNameClubbed'] = df.apply(ClubFunction, axis=1)
+
+        #Group by
+        df = df.groupby(['groupNameClubbed']).size().reset_index(name='count')
+        #Sort
+        df['groupNameClubbed_cat'] = pd.Categorical(
+            df['groupNameClubbed'], 
+            categories=['Intake','Support 1e lijn','Support 2e lijn','Technisch consultants and Functioneel consultants','Development','Others'], 
+            ordered=True
+        )
+        data = df.sort_values('groupNameClubbed_cat')
+
+        #data = data.to_dict()  # convert dataframe to dict
+        json_values = data.to_json(orient ='values')
+        
+        return {'data': json_values}, 200  # return data and 200 OK
+
+                    
+class CSClosedTicketsTicketsByPriority(Resource):
+    def get(self):
+
+        df = pd.read_csv('customerSupport.csv')  # read local CSV
+        #Apply filters
+        df = df[df.status.isin(['Closed', 'In ontwikkeling', 'Resolved'])]
+        df = df[df.message.isin(['Feedback', 'Incident / storing', 'Project', 'Vraag', ])]
+        #Group by
+        data = df.groupby(['priority']).size().reset_index(name='count')
+        #data = data.to_dict()  # convert dataframe to dict
+        json_values = data.to_json(orient ='values')
+        return {'data': json_values}, 200  # return data and 200 OK
+
+                    
+class CSClosedTicketsTicketsBySolutionStatus(Resource):
+    def get(self):
+
+        df = pd.read_csv('customerSupport.csv')  # read local CSV
+        #Apply filters
+        df = df[df.status.isin(['Closed', 'In ontwikkeling', 'Resolved'])]
+        df = df[df.message.isin(['Feedback', 'Incident / storing', 'Project', 'Vraag', ])]
+        #Group by
+        data = df.groupby(['solutionStatus']).size().reset_index(name='count')
+        #data = data.to_dict()  # convert dataframe to dict
+        json_values = data.to_json(orient ='values')
+        return {'data': json_values}, 200  # return data and 200 OK
+
+                    
+class CSClosedTicketsTicketsByCompanyname(Resource):
+    def get(self):
+
+        df = pd.read_csv('customerSupport.csv')  # read local CSV
+        #Apply filters
+        df = df[df.status.isin(['Closed', 'In ontwikkeling', 'Resolved'])]
+        df = df[df.message.isin(['Feedback', 'Incident / storing', 'Project', 'Vraag', ])]
+        #Group by
+        data = df.groupby(['companyName']).size().reset_index(name='count')
+        data = data.to_dict()  # convert dataframe to dict
+        
+        return {'data': data}, 200  # return data and 200 OK
+
+
 api.add_resource(CustomerSatisfactionTeams, '/customerSatisfactionTeams')  # add endpoints
 api.add_resource(CustomerSatisfactionEmployees, '/customerSatisfactionEmployees')  # add endpoints
 
@@ -212,6 +316,12 @@ api.add_resource(CSAllOpenTicketsTicketsByAgentMessage, '/CSAllOpenTicketsTicket
 api.add_resource(CSAllOpenTicketsTicketsByTypeMessage, '/CSAllOpenTicketsTicketsByTypeMessage')  # add endpoints
 api.add_resource(CSAllOpenTicketsTicketsByTypePriority, '/CSAllOpenTicketsTicketsByTypePriority')  # add endpoints
 api.add_resource(CSAllOpenTicketsTicketsByCompanyname, '/CSAllOpenTicketsTicketsByCompanyname')  # add endpoints
+
+api.add_resource(CSClosedTicketsTicketsByAgentMessage, '/CSClosedTicketsTicketsByAgentMessage')  # add endpoints
+api.add_resource(CSClosedTicketsTicketsByGroup, '/CSClosedTicketsTicketsByGroup')  # add endpoints
+api.add_resource(CSClosedTicketsTicketsByPriority, '/CSClosedTicketsTicketsByPriority')  # add endpoints
+api.add_resource(CSClosedTicketsTicketsBySolutionStatus, '/CSClosedTicketsTicketsBySolutionStatus')  # add endpoints
+api.add_resource(CSClosedTicketsTicketsByCompanyname, '/CSClosedTicketsTicketsByCompanyname')  # add endpoints
 
 if __name__ == '__main__':
     app.run()  # run our Flask app
