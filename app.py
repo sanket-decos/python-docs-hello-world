4cd71964e2ddca3f68b4e9f4fd1b168a12247597
sanket-decos <86230483+sanket-decos@users.noreply.github.com>
2021-06-28 14:41:06 +0530

    Update 28 June

--- app.py
+++ app.py
@@ -104,193 +104,65 @@ class CSOpenIncidents(Resource):
         
         return {'data': data}, 200  # return data and 200 OK
 
-                    
-class CSOpenIncidentsTicketsByType(Resource):
-    def get(self):
-
-        df = pd.read_csv('customerSupport.csv')  # read local CSV
-        #Apply filters
-        df = df[df.status.isin(['Bij Development', 'In afwachting van externe partij', 'Intern informatie opgevraagd', 'Open', 'Pending', 'Wachten op klantreactie', 'Werkzaamheden ingepland'])]
-        df = df.loc[(df['message'] == 'Incident / storing')]
-        #Group by
-        data = df.groupby(['type']).size().reset_index(name='count')
-        data = data.to_dict()  # convert dataframe to dict
-        return {'data': data}, 200  # return data and 200 OK
-                    
-class CSOpenIncidentsTicketsByAgent(Resource):
-    def get(self):
-
-        df = pd.read_csv('customerSupport.csv')  # read local CSV
-        #Apply filters
-        df = df[df.status.isin(['Bij Development', 'In afwachting van externe partij', 'Intern informatie opgevraagd', 'Open', 'Pending', 'Wachten op klantreactie', 'Werkzaamheden ingepland'])]
-        df = df.loc[(df['message'] == 'Incident / storing')]
-        #Group by
-        data = df.groupby(['agent']).size().reset_index(name='count')
-        data = data.to_dict()  # convert dataframe to dict
-        return {'data': data}, 200  # return data and 200 OK
-                    
-class CSOpenIncidentsTicketsByTypePriority(Resource):
-    def get(self):
-
-        df = pd.read_csv('customerSupport.csv')  # read local CSV
-        #Apply filters
-        df = df[df.status.isin(['Bij Development', 'In afwachting van externe partij', 'Intern informatie opgevraagd', 'Open', 'Pending', 'Wachten op klantreactie', 'Werkzaamheden ingepland'])]
-        df = df.loc[(df['message'] == 'Incident / storing')]
-        #Group by
-        datagroup = df.groupby(['type', 'priority']).size().reset_index(name='countt')
-        pivoted = datagroup.pivot(index='type', columns= 'priority', values='countt')
-        flattened = pd.DataFrame(pivoted.to_records()).fillna(0)
-
-        data = flattened.to_dict()  # convert dataframe to dict
-        return {'data': data}, 200  # return data and 200 OK
-                    
-class CSOpenIncidentsTicketsByStatus(Resource):
-    def get(self):
-
-        df = pd.read_csv('customerSupport.csv')  # read local CSV
-        #Apply filters
-        df = df[df.status.isin(['Bij Development', 'In afwachting van externe partij', 'Intern informatie opgevraagd', 'Open', 'Pending', 'Wachten op klantreactie', 'Werkzaamheden ingepland'])]
-        df = df.loc[(df['message'] == 'Incident / storing')]
-        #Group by
-        data = df.groupby(['status']).size().reset_index(name='count')
-        #data = data.to_dict()  # convert dataframe to dict
-        json_values = data.to_json(orient ='values')
-        return {'data': json_values}, 200  # return data and 200 OK
-                    
-class CSOpenIncidentsTicketsByGroup(Resource):
-    def get(self):
-
-        df = pd.read_csv('customerSupport.csv')  # read local CSV
-        #Apply filters
-        df = df[df.status.isin(['Bij Development', 'In afwachting van externe partij', 'Intern informatie opgevraagd', 'Open', 'Pending', 'Wachten op klantreactie', 'Werkzaamheden ingepland'])]
-        df = df.loc[(df['message'] == 'Incident / storing')]
-
-        def ClubFunction(row):
-            if row['groupName'] == 'Development':
-                groupNameClubbed = 'Development'
-            elif row['groupName'] == 'Technisch consultants':
-                groupNameClubbed = 'Technisch consultants and Functioneel consultants'
-            elif row['groupName'] == 'Functioneel consultants':
-                groupNameClubbed = 'Technisch consultants and Functioneel consultants'
-            elif row['groupName'] == 'Intake':
-                groupNameClubbed = 'Intake'
-            elif row['groupName'] == 'India Intake':
-                groupNameClubbed = 'Intake'
-            elif row['groupName'] == 'Support 1e lijn':
-                groupNameClubbed = 'Support 1e lijn'
-            elif row['groupName'] == 'Support 2e lijn':
-                groupNameClubbed = 'Support 2e lijn'
-            else:
-                groupNameClubbed = 'Others'
-            return groupNameClubbed
-        df['groupNameClubbed'] = df.apply(ClubFunction, axis=1)
-
-        #Group by
-        df = df.groupby(['groupNameClubbed']).size().reset_index(name='count')
-        #Sort
-        df['groupNameClubbed_cat'] = pd.Categorical(
-            df['groupNameClubbed'], 
-            categories=['Intake','Support 1e lijn','Support 2e lijn','Technisch consultants and Functioneel consultants','Development','Others'], 
-            ordered=True
-        )
-        data = df[['groupNameClubbed_cat', 'count']].sort_values('groupNameClubbed_cat')
-
-        #data = data.to_dict()  # convert dataframe to dict
-        json_values = data.to_json(orient ='values')
-        return {'data': json_values}, 200  # return data and 200 OK
-
-
 
 
                     
-class CSAllOpenTicketsTicketsByAgentMessage(Resource):
+class CSAllOpenTickets(Resource):
     def get(self):
 
         df = pd.read_csv('customerSupport.csv')  # read local CSV
         #Apply filters
         df = df[df.status.isin(['Bij Development', 'In afwachting van externe partij', 'Intern informatie opgevraagd', 'Open', 'Pending', 'Wachten op klantreactie', 'Werkzaamheden ingepland'])]
         df = df[~df.message.isin(['Project', ''])]
-        #Group by
+
+        data = {}
+
+        #TicketsByAgentMessage
         datagroup = df.groupby(['agent', 'message']).size().reset_index(name='countt')
         pivoted = datagroup.pivot(index='agent', columns= 'message', values='countt')
         flattened = pd.DataFrame(pivoted.to_records()).fillna(0)
-        data = flattened.to_dict()  # convert dataframe to dict
-
-        return {'data': data}, 200  # return data and 200 OK
-
-                    
-class CSAllOpenTicketsTicketsByTypeMessage(Resource):
-    def get(self):
-
-        df = pd.read_csv('customerSupport.csv')  # read local CSV
-        #Apply filters
-        df = df[df.status.isin(['Bij Development', 'In afwachting van externe partij', 'Intern informatie opgevraagd', 'Open', 'Pending', 'Wachten op klantreactie', 'Werkzaamheden ingepland'])]
-        df = df[~df.message.isin(['Project', ''])]
-        #Group by
+        data['TicketsByAgentMessage'] = flattened.to_dict()  # convert dataframe to dict
+        
+        #TicketsByTypeMessage
         datagroup = df.groupby(['type', 'message']).size().reset_index(name='countt')
         pivoted = datagroup.pivot(index='type', columns= 'message', values='countt')
         flattened = pd.DataFrame(pivoted.to_records()).fillna(0)
-        data = flattened.to_dict()  # convert dataframe to dict
+        data['TicketsByTypeMessage'] = flattened.to_dict()  # convert dataframe to dict
         
-        return {'data': data}, 200  # return data and 200 OK
-
-                    
-class CSAllOpenTicketsTicketsByTypePriority(Resource):
-    def get(self):
-
-        df = pd.read_csv('customerSupport.csv')  # read local CSV
-        #Apply filters
-        df = df[df.status.isin(['Bij Development', 'In afwachting van externe partij', 'Intern informatie opgevraagd', 'Open', 'Pending', 'Wachten op klantreactie', 'Werkzaamheden ingepland'])]
-        df = df[~df.message.isin(['Project', ''])]
-        #Group by
+        #TicketsByTypePriority
         datagroup = df.groupby(['type', 'priority']).size().reset_index(name='countt')
         pivoted = datagroup.pivot(index='type', columns= 'priority', values='countt')
         flattened = pd.DataFrame(pivoted.to_records()).fillna(0)
-        data = flattened.to_dict()  # convert dataframe to dict
+        data['TicketsByTypePriority'] = flattened.to_dict()  # convert dataframe to dict
         
-        return {'data': data}, 200  # return data and 200 OK
+        #TicketsByCompanyname
+        dataTicketsByCompanyname = df.groupby(['companyName']).size().reset_index(name='count')
+        data['TicketsByCompanyname'] = dataTicketsByCompanyname.to_dict()  # convert dataframe to dict
 
-                    
-class CSAllOpenTicketsTicketsByCompanyname(Resource):
-    def get(self):
-
-        df = pd.read_csv('customerSupport.csv')  # read local CSV
-        #Apply filters
-        df = df[df.status.isin(['Bij Development', 'In afwachting van externe partij', 'Intern informatie opgevraagd', 'Open', 'Pending', 'Wachten op klantreactie', 'Werkzaamheden ingepland'])]
-        df = df[~df.message.isin(['Project', ''])]
-        #Group by
-        data = df.groupby(['companyName']).size().reset_index(name='count')
-        data = data.to_dict()  # convert dataframe to dict
-        
         return {'data': data}, 200  # return data and 200 OK
 
 
 
 
-class CSClosedTicketsTicketsByAgentMessage(Resource):
+
+class CSClosedTickets(Resource):
     def get(self):
 
         df = pd.read_csv('customerSupport.csv')  # read local CSV
         #Apply filters
         df = df[df.status.isin(['Closed', 'In ontwikkeling', 'Resolved'])]
         df = df[df.message.isin(['Feedback', 'Incident / storing', 'Project', 'Vraag', ])]
-        #Group by
+
+        data = {}
+
+        #TicketsByAgentMessage
         datagroup = df.groupby(['agent', 'message']).size().reset_index(name='countt')
         pivoted = datagroup.pivot(index='agent', columns= 'message', values='countt')
         flattened = pd.DataFrame(pivoted.to_records()).fillna(0)
-        data = flattened.to_dict()  # convert dataframe to dict
+        data['TicketsByAgentMessage'] = flattened.to_dict()  # convert dataframe to dict
         
-        return {'data': data}, 200  # return data and 200 OK
 
-                    
-class CSClosedTicketsTicketsByGroup(Resource):
-    def get(self):
-
-        df = pd.read_csv('customerSupport.csv')  # read local CSV
-        #Apply filters
-        df = df[df.status.isin(['Closed', 'In ontwikkeling', 'Resolved'])]
-        df = df[df.message.isin(['Feedback', 'Incident / storing', 'Project', 'Vraag', ])]
-
+        #TicketsByGroup
         def ClubFunction(row):
             if row['groupName'] == 'Development':
                 groupNameClubbed = 'Development'
@@ -310,66 +182,35 @@ class CSClosedTicketsTicketsByGroup(Resource):
                 groupNameClubbed = 'Others'
             return groupNameClubbed
         df['groupNameClubbed'] = df.apply(ClubFunction, axis=1)
-
-        #Group by
-        df = df.groupby(['groupNameClubbed']).size().reset_index(name='count')
+        
+        dfTicketsByGroup = df.groupby(['groupNameClubbed']).size().reset_index(name='count')
         #Sort
-        df['groupNameClubbed_cat'] = pd.Categorical(
-            df['groupNameClubbed'], 
+        dfTicketsByGroup['groupNameClubbed_cat'] = pd.Categorical(
+            dfTicketsByGroup['groupNameClubbed'], 
             categories=['Intake','Support 1e lijn','Support 2e lijn','Technisch consultants and Functioneel consultants','Development','Others'], 
             ordered=True
         )
-        data = df[['groupNameClubbed_cat', 'count']].sort_values('groupNameClubbed_cat')
+        dataTicketsByGroup = dfTicketsByGroup[['groupNameClubbed_cat', 'count']].sort_values('groupNameClubbed_cat')
+        data['TicketsByAgentMessage'] = dataTicketsByGroup.to_json(orient ='values')
 
-        #data = data.to_dict()  # convert dataframe to dict
-        json_values = data.to_json(orient ='values')
+        #TicketsByPriority
+        dataTicketsByPriority = df.groupby(['priority']).size().reset_index(name='count')
+        data['TicketsByPriority'] = dataTicketsByPriority.to_json(orient ='values')
+
+        #TicketsBySolutionStatus
+        dataTicketsBySolutionStatus = df.groupby(['solutionStatus']).size().reset_index(name='count')
+        data['TicketsBySolutionStatus'] = dataTicketsBySolutionStatus.to_json(orient ='values')
         
-        return {'data': json_values}, 200  # return data and 200 OK
-
-                    
-class CSClosedTicketsTicketsByPriority(Resource):
-    def get(self):
-
-        df = pd.read_csv('customerSupport.csv')  # read local CSV
-        #Apply filters
-        df = df[df.status.isin(['Closed', 'In ontwikkeling', 'Resolved'])]
-        df = df[df.message.isin(['Feedback', 'Incident / storing', 'Project', 'Vraag', ])]
-        #Group by
-        data = df.groupby(['priority']).size().reset_index(name='count')
-        #data = data.to_dict()  # convert dataframe to dict
-        json_values = data.to_json(orient ='values')
-        return {'data': json_values}, 200  # return data and 200 OK
-
-                    
-class CSClosedTicketsTicketsBySolutionStatus(Resource):
-    def get(self):
-
-        df = pd.read_csv('customerSupport.csv')  # read local CSV
-        #Apply filters
-        df = df[df.status.isin(['Closed', 'In ontwikkeling', 'Resolved'])]
-        df = df[df.message.isin(['Feedback', 'Incident / storing', 'Project', 'Vraag', ])]
-        #Group by
-        data = df.groupby(['solutionStatus']).size().reset_index(name='count')
-        #data = data.to_dict()  # convert dataframe to dict
-        json_values = data.to_json(orient ='values')
-        return {'data': json_values}, 200  # return data and 200 OK
-
-                    
-class CSClosedTicketsTicketsByCompanyname(Resource):
-    def get(self):
-
-        df = pd.read_csv('customerSupport.csv')  # read local CSV
-        #Apply filters
-        df = df[df.status.isin(['Closed', 'In ontwikkeling', 'Resolved'])]
-        df = df[df.message.isin(['Feedback', 'Incident / storing', 'Project', 'Vraag', ])]
-        #Group by
-        data = df.groupby(['companyName']).size().reset_index(name='count')
-        data = data.to_dict()  # convert dataframe to dict
+        #TicketsByCompanyname
+        dataTicketsByCompanyname = df.groupby(['companyName']).size().reset_index(name='count')
+        data['TicketsByCompanyname'] = dataTicketsByCompanyname.to_dict()  # convert dataframe to dict
         
         return {'data': data}, 200  # return data and 200 OK
 
-                    
-class CSTimeAnalysisTicketsByMonthQuarter(Resource):
+
+
+
+class CSTimeAnalysis(Resource):
     def get(self):
 
         df = pd.read_csv('customerSupportSummary.csv')  # read local CSV
@@ -407,20 +248,12 @@ class CSTimeAnalysisTicketsByMonthQuarter(Resource):
             series.append(temp)
 
         data['seriesDrill'] = series
-
-        #data = data.replace("'", "")        
-        return {'data': data}, 200  # return data and 200 OK
-
-                    
-class CSTimeAnalysisTicketsByWeek(Resource):
-    def get(self):
-
-        df = pd.read_csv('customerSupportSummary.csv')  # read local CSV
-        #Group by
-        data = df.groupby(['Year', 'WeekNumber'])["New", "Closed", "OpenTicketstodate"].sum().reset_index()
         
-        data = data.to_dict()  # convert dataframe to dict
-        
+        #Group by for TimeAnalysisTicketsByWeek
+        dataTicketsByWeek = df.groupby(['Year', 'WeekNumber'])["New", "Closed", "OpenTicketstodate"].sum().reset_index()
+        dataTicketsByWeek = dataTicketsByWeek.to_dict()  # convert dataframe to dict
+        data['TicketsByWeek'] = dataTicketsByWeek
+
         return {'data': data}, 200  # return data and 200 OK
         
                     
@@ -475,25 +308,11 @@ api.add_resource(CustomerSatisfactionEmployees, '/customerSatisfactionEmployees'
 
 api.add_resource(CSOpenIncidents, '/CSOpenIncidents')  # add endpoints
 
-api.add_resource(CSOpenIncidentsTicketsByType, '/CSOpenIncidentsTicketsByType')  # add endpoints
-api.add_resource(CSOpenIncidentsTicketsByAgent, '/CSOpenIncidentsTicketsByAgent')  # add endpoints
-api.add_resource(CSOpenIncidentsTicketsByTypePriority, '/CSOpenIncidentsTicketsByTypePriority')  # add endpoints
-api.add_resource(CSOpenIncidentsTicketsByStatus, '/CSOpenIncidentsTicketsByStatus')  # add endpoints
-api.add_resource(CSOpenIncidentsTicketsByGroup, '/CSOpenIncidentsTicketsByGroup')  # add endpoints
+api.add_resource(CSAllOpenTickets, '/CSAllOpenTickets')  # add endpoints
 
-api.add_resource(CSAllOpenTicketsTicketsByAgentMessage, '/CSAllOpenTicketsTicketsByAgentMessage')  # add endpoints
-api.add_resource(CSAllOpenTicketsTicketsByTypeMessage, '/CSAllOpenTicketsTicketsByTypeMessage')  # add endpoints
-api.add_resource(CSAllOpenTicketsTicketsByTypePriority, '/CSAllOpenTicketsTicketsByTypePriority')  # add endpoints
-api.add_resource(CSAllOpenTicketsTicketsByCompanyname, '/CSAllOpenTicketsTicketsByCompanyname')  # add endpoints
+api.add_resource(CSClosedTickets, '/CSClosedTickets')  # add endpoints
 
-api.add_resource(CSClosedTicketsTicketsByAgentMessage, '/CSClosedTicketsTicketsByAgentMessage')  # add endpoints
-api.add_resource(CSClosedTicketsTicketsByGroup, '/CSClosedTicketsTicketsByGroup')  # add endpoints
-api.add_resource(CSClosedTicketsTicketsByPriority, '/CSClosedTicketsTicketsByPriority')  # add endpoints
-api.add_resource(CSClosedTicketsTicketsBySolutionStatus, '/CSClosedTicketsTicketsBySolutionStatus')  # add endpoints
-api.add_resource(CSClosedTicketsTicketsByCompanyname, '/CSClosedTicketsTicketsByCompanyname')  # add endpoints
-
-api.add_resource(CSTimeAnalysisTicketsByMonthQuarter, '/CSTimeAnalysisTicketsByMonthQuarter')  # add endpoints
-api.add_resource(CSTimeAnalysisTicketsByWeek, '/CSTimeAnalysisTicketsByWeek')  # add endpoints
+api.add_resource(CSTimeAnalysis, '/CSTimeAnalysis')  # add endpoints
 
 api.add_resource(CSSLAOpenTickets, '/CSSLAOpenTickets')  # add endpoints
 api.add_resource(CSSLAClosedTickets, '/CSSLAClosedTickets')  # add endpoints
